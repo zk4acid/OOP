2222ff52c101705fc8580f54d7971ffd966575cb
unknown <todor.yordanov@mail.bg>
2015-11-19 14:20:07 +0200

    Homework

---
+++ 01. DefiningClasses/01. Person/MainProgram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Persons
+{
+    class MainProgram
+    {
+        static void Main()
+        {
+            Person pesho = new Person("Pesho", 22, "Pesh@abv.bg");
+            Console.WriteLine(pesho.ToString());
+        }
+    }
+}
---
+++ 01. DefiningClasses/02. LaptopShop/Battery.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace LaptopShop
+{
+    class Battery
+    {
+        private string batteryModel;
+        private double batteryLife;
+
+        public Battery(string batteryModel, double batteryLife)
+        {
+            this.BatteryModel = batteryModel;
+            this.BatteryLife = batteryLife;
+        }
+
+        public string BatteryModel
+        {
+            get
+            {
+                return this.batteryModel;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.batteryModel = value;
+                }
+                else
+                {
+                    throw new Exception("Empty string");
+                }
+            }
+        }
+
+        public double BatteryLife
+        {
+            get
+            {
+                return this.batteryLife;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.batteryLife = value;
+                }
+                else
+                {
+                    throw new Exception("Numeric data cannot be negative");
+                }
+            }
+        }
+    }
+}
