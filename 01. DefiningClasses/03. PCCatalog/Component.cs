fe0836755e391f36c4cbe7e6daed5c14d13b2dc2
unknown <todor.yordanov@mail.bg>
2015-11-22 23:22:52 +0200

    Homework

---
+++ 01. DefiningClasses/03. PCCatalog/Component.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace PCCatalog
+{
+    class Component
+    {
+        private string name;
+        private decimal price;
+
+        public Component(string name, decimal price)
+        {
+            this.Name = name;
+            this.Price = price;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new Exception("Empty string");
+                }
+            }
+        }
+
+        public decimal Price
+        {
+            get
+            {
+                return this.price;
+            }
+            set
+            {
+                if (value >=0)
+                {
+                    this.price = value;
+                }
+                else
+                {
+                    throw new Exception("Negative price");
+                }
+            }
+        }
+    }
+}
---
+++ 01. DefiningClasses/03. PCCatalog/Computer.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace PCCatalog
+{
+    class Computer
+    {
+        private string name;
+        Component computerProcessor;
+        Component computerGraphicsCard;
+        Component computerMotherboard;
+
+        public Computer(string name, Component processor, Component graphicsCard, Component motherboard)
+        {
+            this.Name = name;
+            computerProcessor = processor;
+            computerGraphicsCard = graphicsCard;
+            computerMotherboard = motherboard;
+        }
+
+        public Computer(string name) : this(name, null, null, null)
+        {
+            this.Name = name;
+        }
+
+        public Computer(string name, Component processor, decimal price) : this(name, processor, null, null)
+        {
+            this.Name = name;
+            computerProcessor = processor;
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (value != string.Empty)
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    throw new Exception("Empty string");
+                }
+            }
+        }
+
+        public decimal CalculateComputerPrice()
+        {
+            decimal totalComputerPrice = 0;
+            if (computerProcessor.Price >= 0)
+            {
+                totalComputerPrice += computerProcessor.Price;
+            }
+            if (computerGraphicsCard.Price >= 0)
+            {
+                totalComputerPrice += computerGraphicsCard.Price;
+            }
+            if (computerMotherboard.Price >= 0)
+            {
+                totalComputerPrice += computerMotherboard.Price;
+            }
+
+            return totalComputerPrice;
+        }
+
+        public void PrintComputerInformation()
+        {
+            Console.WriteLine(this.Name);
+            if (computerProcessor.Price > 0)
+            {
+                Console.WriteLine("--> {0} price: {1:c}", computerProcessor.Name, computerProcessor.Price);
+            }
+            if (computerGraphicsCard.Price > 0)
+            {
+                Console.WriteLine("--> {0} price: {1:c}", computerGraphicsCard.Name, computerGraphicsCard.Price);
+            }
+            if (computerMotherboard.Price > 0)
+            {
+                Console.WriteLine("--> {0} price: {1:c}", computerMotherboard.Name, computerMotherboard.Price);
+            }
+            if (this.CalculateComputerPrice() > 0)
+            {
+                Console.WriteLine("Total Price: {0:c}", this.CalculateComputerPrice());
+            }         
+        }
+    }
+}
